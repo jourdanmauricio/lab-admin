9f983014d4de79eae6363b0ce60f91fc26f638c0
Mauricio <jourdanmauricio@gmail.com>
2022-07-13 19:08:03 -0300

    Add chage code for token

--- src/routes/settings/meli-callback.svelte
+++ src/routes/settings/meli-callback.svelte
@@ -1,11 +1,24 @@
 <script>
   import Spinner from "./../../lib/Spinner.svelte";
   import { page } from "$app/stores";
+  import { replaceCode } from "./../../services/api/userMl.js";
+  import { onMount } from "svelte";
+  import { notification } from "../../store/stores";
 
   const code = $page.url.searchParams.get("code");
   const state = $page.url.searchParams.get("state");
+  let data = null;
 
   let isLoading = true;
+
+  onMount(() => {
+    try {
+      data = replaceCode(code, state);
+      console.log("data", data);
+    } catch (error) {
+      notification.show(error, "error");
+    }
+  });
 </script>
 
 <h1>Estamos sincronizando con Mercado Libre....</h1>
--- src/services/api/userMl.js
+++ src/services/api/userMl.js
@@ -1,6 +1,8 @@
 import Api from "../Api";
 import { credentials } from "../../store/stores";
+import { variables } from "../../lib/variables";
 import { get } from "svelte/store";
+import axios from "axios";
 
 export const authMl = async (data) => {
   try {
@@ -39,6 +41,38 @@ export const delUserMl = async () => {
     const response = await Api.delete(`/usersml/${user.userMl.id}`);
     return response;
   } catch (error) {
-    console.log(error);
+    console.log("error", error);
+    let message = "";
+    message = error.response.data
+      ? `${error.response.data.statusCode}: ${error.response.data.message}`
+      : "Error eliminando usuario ML 😞";
+    throw message;
+  }
+};
+
+export const replaceCode = async (code, state) => {
+  try {
+    // GET ML -> change code for access_token
+
+    const credentialsMl = await axios({
+      method: "post",
+      url: `${config.mlApi}/oauth/token`,
+      data: {
+        grant_type: "authorization_code",
+        client_id: variables.mlAppId,
+        client_secret: variables.mlSecret,
+        code: code,
+        redirect_uri: `${variables.frontend}/settings/meli-callback`,
+      },
+    });
+    // const rta = await service.update(state, resMl);
+    console.log("CredentialsMl: ", credentialsMl);
+  } catch (error) {
+    console.log("error", error);
+    let message = "";
+    message = error.response.data
+      ? `${error.response.data.statusCode}: ${error.response.data.message}`
+      : "Error obteniendo token ML 😞";
+    throw message;
   }
 };
