7877c5afd2a4dca24c558c552adf23063c24e3d8
Mauricio <jourdanmauricio@gmail.com>
2022-07-13 23:52:23 -0300

    Fix

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,6 +1,6 @@
 export const variables = {
   basePath: import.meta.env.VITE_BACKEND,
-  basePathML: import.meta.env.VITE_API_ML,
+  basePathMl: import.meta.env.VITE_API_ML,
   mlAppId: import.meta.env.VITE_ML_APP_ID,
   mlSecret: import.meta.env.VITE_ML_SECRET,
   frontend: import.meta.env.VITE_FRONTEND,
